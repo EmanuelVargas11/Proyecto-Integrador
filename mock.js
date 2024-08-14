6fff40ea4e68d628d73909fac03f1aa919609276
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-08-14 13:25:27 -0300

    segui trabajando con el proyecto. Logre algunos objetivos

--- mock.js
+++ mock.js
@@ -1,31 +1,102 @@
+
 const container = document.querySelector('.container')
+
+const buscador = document.querySelector('#buscar')
 
 const url = 'https://66b8d0893ce57325ac7834b0.mockapi.io/producto'
 
 
+// selecionamos el boton para modo oscuro
+const botonModoOscuro = document.querySelector('#modo-oscuro')
+
+botonModoOscuro.innerText = 'Modo-Claro'
 
 
 
-// imprime los datos de la api propia
+
+
+
+
+
+
+
 fetch(url)
-    .then(res => res.json())
-    // .then(data => console.log(data))
+.then(res => res.json())
+
+// .then(data => console.log(data))
+
+//  manipulacion del boton modo-oscuro y que quede guardado en localstorage
+
+
     .then(data =>{
-        let datos  = ' '
-        data.forEach(producto => {
-            datos += `
-                        <h3>${producto.name}</h3>
-                        <p>precio: $${producto.precio}</p>
-                        <img src="${producto.avatar}" alt="${producto.name}" >
-                        <p>${producto.descripcion}</p>
+
+        botonModoOscuro.addEventListener('click', ()=> {
+
+            document.body.classList.toggle('modo-dark');
+            botonModoOscuro.classList.toggle('boton-mode-oscuro')
+
+            
+            
+            
+            if (document.body.classList.contains('modo-dark')) {
+                
+                localStorage.setItem('Modo-Oscuro', 'true')
+                botonModoOscuro.innerText = 'Modo-Oscuro'
+                
+                
+                
+                
+            }else{
+                localStorage.setItem('Modo-Oscuro', 'false')
+                botonModoOscuro.innerText = 'Modo-Claro'
 
 
-
-
-                     `
-
+            }
+            
         })
-        container.innerHTML = datos
-    })
-    .catch(err => console.log(err))
+        
+        if (localStorage.getItem('Modo-Oscuro') === 'true') {
+            botonModoOscuro.classList.add('boton-mode-oscuro')
+            
+            document.body.classList.add('modo-dark')
+            botonModoOscuro.innerText = 'Modo-Oscuro'
+
+
+
+                        
+        }else {
+                                       
+            document.body.classList.remove('modo-dark')
+            botonModoOscuro.innerText = 'Modo-Claro'
+
+        }
+        
+    // variable vacia para luego poder rellenar con los dato que vienen de nuestra api
+    let datos  = ' '
     
+    data.forEach(producto => {
+        // muestra los datos que contenemos en nuestra api y los que deseamos.
+        
+        datos += `
+        <div class="cards">
+
+        <h3>${producto.name}</h3>
+        <p>${producto.descripcion}</p>
+        <img src="${producto.avatar}" alt="${producto.name}" >
+        <p>precio: $${producto.precio}</p>
+        
+        </div>
+        
+        
+        
+        
+        `
+        
+    })
+    // aqui lo imprimer en el html 
+    container.innerHTML = datos
+})
+.catch(err => console.log(err))
+
+
+

cdb0197b1ec328fb6a84cdadb2be16edcd529ef6
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-08-22 22:48:09 -0300

    Se hizo correccion y modificacion visual del codigo

--- mock.js
+++ mock.js
@@ -1,28 +1,26 @@
 import { btnModeDark } from "./modoDark.js"
+
 const container = document.querySelector('.container')
 
 const buscador = document.querySelector('#buscar')
 
 const url = 'https://66b8d0893ce57325ac7834b0.mockapi.io/producto'
 // selecionamos el boton para modo oscuro
-const botonModoOscuro = document.querySelector('#modo-oscuro')
+const boton = document.querySelector('#modo-oscuro')
 // texto para el boton
- const textoBoton = botonModoOscuro.innerText = 'Modo-Claro'
+ const textoBoton = boton.innerText = 'Modo-Claro'
 
 fetch(url)
 .then(res => res.json())
 
 // .then(data => console.log(data))
-
-
-
 .then(data =>{
-    
+    // funcion modo oscuro
+    btnModeDark
         
     // variable vacia para luego poder rellenar con los dato que vienen de nuestra api
     let datos  = ' '
 
-    btnModeDark()
     
     data.forEach(producto => {
         // muestra los datos que contenemos en nuestra api y los que deseamos.
@@ -32,21 +30,14 @@ fetch(url)
 
         <h3>${producto.name}</h3>
         <p>${producto.descripcion}</p>
-        <img src="${producto.avatar}" alt="${producto.name}" >
+        <img  class="image" src="${producto.avatar}" alt="${producto.name}" >
         <p>precio: $${producto.precio}</p>
         
-        </div>
+        </div>      
         
-        
-        
-        
-        `
-        
+        `       
     })
     // aqui lo imprimer en el html 
     container.innerHTML = datos
 })
 .catch(err => console.log(err))
-
-
-
--- modoDark.js
+++ modoDark.js
@@ -1,17 +1,10 @@
 const boton1 = document.querySelector('#modo-oscuro')
 boton1.innerText = 'Modo-Claro'
-
-localStorage.setItem("Modo-Oscuro", "true")
-localStorage.setItem("Modo-Oscuro", "false")
-
-// localStorage.getItem("Modo-Oscuro")
-localStorage.getItem("Modo-Oscuro", "false")
-localStorage.getItem("Modo-Oscuro", "true")
-
-
+boton1.addEventListener('click',btnModeDark)
 
 export function btnModeDark () {
-    
+
+    let recupera = localStorage.getItem("Modo-Oscuro", "true")
     
     // seleccion del body mediante toggle
     document.body.classList.toggle('fondo-modo-false')
@@ -19,10 +12,13 @@ export function btnModeDark () {
     boton1.classList.toggle('fondo-modo-true')
     
     // condicional para que guarde en localstorage    
-    
     if (document.body.classList.contains('fondo-modo-true')) {
         localStorage.setItem("Modo-Oscuro", "true")
+        if (recupera) {
 
+            document.body.classList.add('fondo-modo-oscuro')
+            boton1.classList.add('fondo-modo-true')            
+        }
         boton1.innerText = 'Modo-Oscuro'
         console.log('click1');
         
@@ -30,50 +26,8 @@ export function btnModeDark () {
         boton1.innerText = 'Modo-Claro'
         localStorage.setItem("Modo-Oscuro", "false")
         console.log('click2');
-        
-}
-    if (localStorage.getItem("Modo-Oscuro") === "true") {
-        
-        boton1.classList.add('fondo-modo-true')            
-        document.body.classList.add('fondo-modo-oscuro')
-        boton1.innerText = 'Modo-Oscuro'
-        console.log('me toma1?');
-                
-    }else {
         localStorage.getItem("Modo-Oscuro", "false")
-        
         document.body.classList.remove('fondo-modo-true')
-        // boton1.classList.remove('fondo-modo-true')
-        boton1.innerText = 'Modo-Claro'
-        // console.log('recupera', localStorage.getItem("Modo-Oscuro")  );
-    }
-    boton1.addEventListener('click',btnModeDark)
+        
 }
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
